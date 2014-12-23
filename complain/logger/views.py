bebb6bf80cd601ac988ca2efdcf39b0c625b371d
EricSchles <ericschles@gmail.com>
2014-12-23 14:42:09 -0800

    almost done

--- complain/logger/views.py
+++ complain/logger/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from logger.models import Complaint
 from django.http import HttpResponse
-
+from datetime import datetime
 # Create your views here.
 def index(request):
+    #to do: make sure picture has picture extensions only.
+    complaint = Complaint(
+        text_box=request.GET.get('text_box'),
+        screen_shot=request.GET.get("screen_shot"),
+        original_document=request.GET.get("original_document"),
+        pub_date = datetime.now()
+    )
+    complaint.save()
     return render(request, "logger/index.html")
 
 def log(request):
