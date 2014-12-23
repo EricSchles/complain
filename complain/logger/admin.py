8ebb959aed54bfcdb11976b00512d0d4f8ab223c
EricSchles <ericschles@gmail.com>
2014-12-23 12:10:53 -0800

    adding filter by date

--- complain/logger/admin.py
+++ complain/logger/admin.py
@@ -3,5 +3,6 @@ from logger.models import Complaint
 
 class ComplaintAdmin(admin.ModelAdmin):
     list_display = ("complain_id","text_box","pub_date")
+    list_filter = ["pub_date"]
 # Register your models here.
 admin.site.register(Complaint,ComplaintAdmin)
