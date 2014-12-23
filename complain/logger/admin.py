9f9cd6f347077ab74d03f868a4df2c0bcc20863c
EricSchles <ericschles@gmail.com>
2014-12-23 12:11:29 -0800

    adding search by text

--- complain/logger/admin.py
+++ complain/logger/admin.py
@@ -4,5 +4,6 @@ from logger.models import Complaint
 class ComplaintAdmin(admin.ModelAdmin):
     list_display = ("complain_id","text_box","pub_date")
     list_filter = ["pub_date"]
+    search_fields = ["text_box"]
 # Register your models here.
 admin.site.register(Complaint,ComplaintAdmin)
