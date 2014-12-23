e00675e54e0db6d33b4073e01326e68adced985f
EricSchles <ericschles@gmail.com>
2014-12-23 12:10:07 -0800

    adding display list

--- complain/logger/admin.py
+++ complain/logger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from logger.models import Complaint
 
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ("complain_id","text_box","pub_date")
 # Register your models here.
-admin.site.register(Complaint)
+admin.site.register(Complaint,ComplaintAdmin)
