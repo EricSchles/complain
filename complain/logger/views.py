d46bf78d8e4fcd1d654aabdfcfd6a402d7c523f1
EricSchles <ericschles@gmail.com>
2014-12-23 12:16:03 -0800

    added views

--- complain/logger/views.py
+++ complain/logger/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from logger.models import Complaint
 
 # Create your views here.
+def index(request):
+    return render(request, "logger/index.html")
+
+def log(request):
+    data = Complaint()
+    #add sending data to the database
+    return render(request, "logger/success.html")
