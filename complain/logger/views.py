24c35e9f532639db3b3223b91e4b67e21f23af1e
EricSchles <ericschles@gmail.com>
2014-12-29 11:29:58 -0500

    updating views to do correct thing for file upload

--- complain/logger/views.py
+++ complain/logger/views.py
@@ -4,20 +4,24 @@ from django.http import HttpResponse
 from datetime import datetime
 
 
+
+
 # Create your views here.
 def index(request):
     #to do: make sure picture has picture extensions only.
     
-    # print request.GET.get("screen_shot")
-    # complaint = Complaint(
-    #     text_box=request.GET.get('text_box'),
-    #     screen_shot=request.GET.get("screen_shot"),
-    #     original_document=request.GET.get("original_document"),
-    #     pub_date = datetime.now()
-    # )
-    # if complaint.text_box != None:
-    #     complaint.save()
+    print request.GET.get("screen_shot")
+    if request.method == 'POST':
+        complaint = Complaint(
+            text_box=request.GET.get('text_box'),
+            screen_shot=request.FILES["screen_shot"],
+            original_document=request.FILES["original_document"],
+            pub_date = datetime.now()
+        )
+        if complaint.text_box != None:
+            complaint.save()
+        
+        
 
-    
     return render(request, "logger/index.html")
 
