deef122d0d03961f09f0e4217e0cadf68de096ca
EricSchles <ericschles@gmail.com>
2014-12-29 10:47:41 -0500

    adding media root

--- complain/complain/settings.py
+++ complain/complain/settings.py
@@ -78,7 +78,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+#Absolute filesystem path is: /home/Documents/complain/media
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
