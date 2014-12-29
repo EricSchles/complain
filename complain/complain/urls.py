30d2f6d3c3182411c83b596b3e3189ee47d3c9f8
EricSchles <ericschles@gmail.com>
2014-12-29 12:40:06 -0500

    updating project wide urls

--- complain/complain/urls.py
+++ complain/complain/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = patterns('',
     url(r'^logger', include('logger.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
